1f8db555c607bba3619d2f5640e1132fff801f9a
Filipe <filipeseventeen1@gmail.com>
2021-12-23 16:45:42 -0300

    Adicionado uma função de atualizar o aplicativo quando ele for aberto

--- App.tsx
+++ App.tsx
@@ -2,7 +2,24 @@ import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'react-native'
 import { Routes } from './src/routes'
 
+import * as Updates from 'expo-updates'
+import { useEffect } from 'react'
+
 export default function App() {
+  useEffect(() => {
+    const updateApp = async () => {
+      const { isAvailable } = await Updates.checkForUpdateAsync()
+
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync()
+
+        await Updates.reloadAsync()
+      }
+    }
+
+    updateApp()
+  }, [])
+
   return (
     <NavigationContainer>
       <StatusBar hidden={true} />
